13a72b04b1b45f820071499de1c366558f18d5e1
Dries <SDries@users.noreply.github.com>
2019-07-04 09:23:47 +0200

    Update safe.pipe.spec.ts
    This is a basic setup for the tests.
    I like the pipe but my project requires all tests to pass.

--- src/app/safe.pipe.spec.ts
+++ src/app/safe.pipe.spec.ts
@@ -1,8 +1,23 @@
 import { SafePipe } from './safe.pipe';
+import {TestBed} from '@angular/core/testing';
+import {DomSanitizer} from '@angular/platform-browser';
 
 describe('SafePipe', () => {
+  let pipe: SafePipe;
+  let domSanitizer: DomSanitizer;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        DomSanitizer
+      ]
+    });
+
+    domSanitizer = TestBed.get(DomSanitizer);
+  });
+
   it('create an instance', () => {
-    const pipe = new SafePipe();
+    pipe = new SafePipe(domSanitizer);
     expect(pipe).toBeTruthy();
   });
 });
